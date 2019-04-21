1b10f95a35910cf130593236261a3e890a23a5f0
Bryce Tucker <41135333+Bryce-Tucker@users.noreply.github.com>
2019-04-21 05:09:16 -0600

    Create ch_8_-_extending_the_multiclipboard.py

---
+++ ch_8_-_extending_the_multiclipboard.py
@@ -0,0 +1,27 @@
+#! python3
+# mcb.pyw - Saves and loads pieces of text to the clipboard
+# Usage: py.exe mcb.pyw save <keyword> - Saves clipboard to keyword.
+#        py.exe mcb.pyw <keyword> - Loads keyword to clipboard.
+#        py.exe mcb.pyw list - Loads all keywords to clipboard.
+
+import shelve, pyperclip, sys
+
+mcb_shelf = shelve.open('mcb')
+
+#Save clipboard content.
+if len(sys.argv) == 3:
+    if sys.argv[1].lower() == 'save':
+        mcb_shelf[sys.argv[2]] = pyperclip.paste()
+    elif sys.argv[1].lower() == 'delete':
+        del mcb_shelf[sys.argv[2]]
+elif len(sys.argv) == 2:
+    #List keywords and load content
+    if sys.argv[1].lower() == 'list':
+        pyperclip.copy(str(list(mcb_shelf.keys())))
+    elif sys.argv[1].lower() == 'delete':
+        for k in mcb_shelf:
+            del mcb_shelf[k]
+    elif sys.argv[1] in mcb_shelf:
+        pyperclip.copy(mcb_shelf[sys.argv[1]])
+
+mcb_shelf.close()
